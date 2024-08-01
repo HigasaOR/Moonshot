f5919436f3279b962bc7aec7817c2e51ec990e05
HigasaOR <lee1003094395@gmail.com>
2024-08-02 00:24:38 +0800

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Chien Lee on 2024/8/2.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
